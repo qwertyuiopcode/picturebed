00b3f3546aeab7104196bc3a47d8ec135030e568
qwertyuiopcode <68463349+qwertyuiopcode@users.noreply.github.com>
2021-06-20 21:58:14 +0800

    Update rpi.sh

--- script/bash/rpi.sh
+++ script/bash/rpi.sh
@@ -2,10 +2,6 @@
 sudo deb http://mirrors.tuna.tsinghua.edu.cn/raspbian/raspbian/ buster main non-free contrib rpi
 sudo deb-src http://mirrors.tuna.tsinghua.edu.cn/raspbian/raspbian/ buster main non-free contrib rpi
 sudo deb http://mirrors.tuna.tsinghua.edu.cn/raspberrypi/ buster main ui
-sudo apt-get install --no-install-recommends xserver-xorg -y
-sudo apt-get install --no-install-recommends xinit -y
-sudo apt-get install lightdm -y
-sudo apt-get install raspberrypi-ui-mods -y
 sudo apt-get update
 sudo apt-get dist-upgrade -y
 sudo apt-get install -y dnsmasq hostapd vim python3-flask python3-requests git
